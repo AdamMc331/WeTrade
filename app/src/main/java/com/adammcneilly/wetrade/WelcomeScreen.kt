221508324ae3fe8481c8b45f737467fc8331b970
adammc331 <amcneilly331@gmail.com>
2021-03-24 19:23:21 -0400

    Adding WelcomeScreen.

--- app/src/main/java/com/adammcneilly/wetrade/WelcomeScreen.kt
+++ app/src/main/java/com/adammcneilly/wetrade/WelcomeScreen.kt
@@ -1,13 +1,127 @@
 package com.adammcneilly.wetrade
 
 import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.ButtonDefaults.outlinedButtonColors
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.adammcneilly.wetrade.ui.theme.WeTradeTheme
 
 @Composable
 fun WelcomeScreen() {
+    Box {
+        WelcomeBackground()
 
+        AppLogo(
+            modifier = Modifier
+                .align(Alignment.Center)
+        )
+
+        WelcomeButtonRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(horizontal = 16.dp)
+                .padding(bottom = 32.dp)
+                .height(48.dp)
+        )
+    }
+}
+
+@Composable
+fun WelcomeBackground() {
+    Image(
+        painterResource(id = R.drawable.ic_welcome_bg),
+        contentDescription = null,
+        contentScale = ContentScale.FillBounds,
+        modifier = Modifier
+            .fillMaxSize()
+    )
+}
+
+@Composable
+fun AppLogo(
+    modifier: Modifier,
+) {
+    Image(
+        painterResource(id = R.drawable.ic_logo),
+        contentDescription = "WeTrade Logo",
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun GetStartedButton(modifier: Modifier) {
+    Button(
+        onClick = { /*TODO*/ },
+        shape = MaterialTheme.shapes.large,
+        modifier = modifier
+    ) {
+        Text("GET STARTED")
+    }
+}
+
+@Composable
+fun LogInButton(modifier: Modifier) {
+    val buttonColors = outlinedButtonColors(
+        backgroundColor = Color.Transparent,
+    )
+
+    val buttonBorder = BorderStroke(
+        width = 1.dp,
+        color = MaterialTheme.colors.primary,
+    )
+
+    OutlinedButton(
+        onClick = { /*TODO*/ },
+        shape = MaterialTheme.shapes.large,
+        colors = buttonColors,
+        border = buttonBorder,
+        modifier = modifier,
+    ) {
+        Text("LOG IN")
+    }
+}
+
+@Composable
+fun WelcomeButtonRow(modifier: Modifier) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier,
+    ) {
+        val buttonModifier = Modifier
+            .weight(1F)
+            .fillMaxHeight()
+
+        GetStartedButton(
+            modifier = buttonModifier
+        )
+
+        LogInButton(
+            modifier = buttonModifier
+        )
+    }
 }
 
 @Preview(
