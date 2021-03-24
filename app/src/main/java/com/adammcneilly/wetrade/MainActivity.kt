5540308d5378bd0ebb8854d03b6b9d1370e43407
adammc331 <amcneilly331@gmail.com>
2021-03-24 00:07:22 -0400

    Stubbing home screen navigation.

--- app/src/main/java/com/adammcneilly/wetrade/MainActivity.kt
+++ app/src/main/java/com/adammcneilly/wetrade/MainActivity.kt
@@ -3,11 +3,9 @@ package com.adammcneilly.wetrade
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.adammcneilly.wetrade.ui.theme.WeTradeTheme
 
 class MainActivity : AppCompatActivity() {
@@ -15,24 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeTradeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+
+                NavHost(navController, "welcome") {
+                    composable("welcome") {
+                        WelcomeScreen()
+                    }
+                    composable("login") {
+                        LoginScreen()
+                    }
+                    composable("home") {
+                        HomeScreen()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WeTradeTheme {
-        Greeting("Android")
     }
 }
